e3b02d5befb795c836d69ffa34bd6a0a041ed750
Naoto Yamada <naoto.yamada@sevenrich.jp>
2024-10-16 21:33:07 +0900

    sheets api sample

---
+++ main.ts
@@ -0,0 +1,89 @@
+import { sheets } from "@googleapis/sheets";
+import { readFile, writeFile } from "fs/promises";
+import { authenticate } from '@google-cloud/local-auth';
+import { OAuth2Client } from "google-auth-library";
+import path from "path";
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await readFile(TOKEN_PATH);
+        return JSON.parse(content.toString());
+    } catch (err) {
+        return null;
+    }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client: OAuth2Client) {
+    const content = await readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content.toString());
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+async function listMajors(auth: any) {
+    const sheetsApi = sheets({ version: 'v4', auth });
+    const res = await sheetsApi.spreadsheets.values.get({
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+    });
+    const rows = res.data.values;
+    if (!rows || rows.length === 0) {
+        console.log('No data found.');
+        return;
+    }
+    console.log('Name, Major:');
+    rows.forEach((row) => {
+        // Print columns A and E, which correspond to indices 0 and 4.
+        console.log(`${row[0]}, ${row[4]}`);
+    });
+}
+
+authorize().then(listMajors).catch(console.error);
